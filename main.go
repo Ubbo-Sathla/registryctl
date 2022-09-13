0586138db6af801077d3b5c5c9e5902f81cfeb8c
panda <843592242@qq.com>
2022-09-14 00:44:10 +0800

    真的 有无聊的 人 会去 研究这个

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/go-containerregistry/pkg/crane"
+	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"io"
+	"os"
+)
+
+func pathOpener(path string) tarball.Opener {
+	return func() (io.ReadCloser, error) {
+		return os.Open(path)
+	}
+}
+func main() {
+	imgTar := "/tmp/registry.tar"
+	fmt.Println("Hello world!")
+	manifests, err := tarball.LoadManifest(pathOpener(imgTar))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(manifests)
+	o := crane.GetOptions()
+
+	for _, manifest := range manifests {
+		for _, tag := range manifest.RepoTags {
+
+			img, err := crane.LoadTag(imgTar, tag)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(tag, img)
+			newTag := fmt.Sprintf("10.127.254.230:5000/library/%s", tag)
+			ref, _ := name.ParseReference(newTag, o.Name...)
+			fmt.Printf("%#v\n", ref)
+			var h v1.Hash
+			switch t := img.(type) {
+			case v1.Image:
+				fmt.Println(ref, "v1.Image", o.Remote)
+				if err := remote.Write(ref, t, o.Remote...); err != nil {
+					fmt.Println(err)
+				}
+				if h, err = t.Digest(); err != nil {
+					fmt.Println(err)
+				}
+			case v1.ImageIndex:
+				fmt.Println(ref, "v1.ImageIndex", o.Remote)
+				if err := remote.WriteIndex(ref, t, o.Remote...); err != nil {
+					fmt.Println(err)
+				}
+				if h, err = t.Digest(); err != nil {
+					fmt.Println(err)
+				}
+			default:
+				fmt.Errorf("cannot push type (%T) to registry", img)
+			}
+			digest := ref.Context().Digest(h.String())
+			fmt.Println(digest)
+		}
+	}
+}
